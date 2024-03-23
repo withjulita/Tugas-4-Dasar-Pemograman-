057b72c86097cbe5a0f2f692bf51981ea926751c
otagapgred <melaniputri1424@gmail.com>
2024-03-24 00:55:05 +0900

    Create Perbandingan.py

---
+++ Perbandingan.py
@@ -0,0 +1,16 @@
+a = 119
+
+biner=bin(a) .replace("b","")
+oktal=oct(a) .replace("0o","")
+dsmal=float(a)
+hexa=hex(a) .replace("0x","")
+
+
+print("Bilangan dalam bentuk Biner : ",biner)
+print("Bilangan dalam bentuk Oktal; : ",oktal)
+print("Bilangan dalam bentuk Desimal : ",dsmal)
+print("Bilangan dalam bentuk Hexadesimal : ",hexa)
+
+
+
+
